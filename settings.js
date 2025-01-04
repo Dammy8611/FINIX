ba64f0ac19ebce1b47ef3c59a6de30e83a20786c
Dtechy <dtechy@dtechy-Lenovo-ideapad-100-15IBY.dtechy.com>
2025-01-04 14:39:34 +0100

    smte

---
+++ settings.js
@@ -0,0 +1,12 @@
+// 𝗖𝗢𝗡𝗧𝗥𝗢𝗟 𝗕𝗢𝗧 
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['2349134622805'] //OWNER
+global.botname = 'FINAL DESTINATION' //BOT NAME
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+//global.baileys2 = require("@adiwajshing/baileys") // NOT CHANGE
+global.creator = "TamaRyuichi" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "idk" //OPSIONAL
+global.wm = "Dtechy" // OPSIONAL
+global.idCH = "120363321780343299@newsletter" // OPSIONAL
